34654ec122e1793ca46b2ca699a15897f215d541
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-02-25 00:15:38 +0300

    Add profiles app

--- shop/profiles/views.py
+++ shop/profiles/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    logger.info(request.GET, request.POST)
-    return HttpResponse("Shop index view")
+def profiles(request):
+    if request.GET.get("param"):
+        logger.info(f"My param = {request.GET.get('param')}")
+    return HttpResponse("Profiles view")
