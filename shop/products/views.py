d51cd0cf619da8c1a43b4dd50778f6867b662871
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-02-25 00:15:16 +0300

    Add product app

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,9 +1,14 @@
+
 import logging
 from django.http import HttpResponse
+
+from shop import settings
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    logger.info(request.GET, request.POST)
+    if request.GET.get("param"):
+        logger.info(f'My custom var = {settings.MY_CUSTOM_VARIABLE}')
+        logger.info(f"My param = {request.GET.get('param')}")
     return HttpResponse("Shop index view")
