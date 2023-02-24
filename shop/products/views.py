3098e071497041427e507d91cfbea825f0e81667
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-02-25 01:02:27 +0300

    Update products app

--- shop/products/views.py
+++ shop/products/views.py
@@ -11,4 +11,10 @@ def index(request):
     if request.GET.get("param"):
         logger.info(f'My custom var = {settings.MY_CUSTOM_VARIABLE}')
         logger.info(f"My param = {request.GET.get('param')}")
+        logger.info(f'Debug type = {type(settings.DEBUG)}')
+        logger.info(f'Debug = {settings.DEBUG}')
+    if settings.FIRST_SETTINGS == 'second':
+        logger.info(f'Settings number = {settings.SECOND_SETTINGS}')
+    if settings.FIRST_SETTINGS == 'third':
+        logger.info(f'Ssttings number = {settings.THIRD_SETTINGS}')
     return HttpResponse("Shop index view")
