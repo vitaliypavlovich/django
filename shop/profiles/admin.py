5f5c96b05653335e4943840153d83c9cab35e0f9
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-02-27 21:59:51 +0300

    Update profiles

--- shop/profiles/admin.py
+++ shop/profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from profiles.models import Profile
+
+@admin.register(Profile)
+class ProductAdmin(admin.ModelAdmin):
+   list_display = ("user", "first_name", "last_name", "created_at")
+   fields = ("user", "first_name", "last_name", 'age', "created_at")
+   readonly_fields = ("created_at",)
+   search_fields = ("first_name", "last_name")
--- shop/profiles/models.py
+++ shop/profiles/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
