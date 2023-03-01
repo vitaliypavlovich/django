33eb8ef5e6ad9bcaf746baeab931233822f298f8
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-02 00:06:22 +0300

    Update profiles

--- shop/profiles/admin.py
+++ shop/profiles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from profiles.models import Profile
+from profiles.models import Profile, Address
 
 @admin.register(Profile)
 class ProductAdmin(admin.ModelAdmin):
@@ -8,3 +8,10 @@ class ProductAdmin(admin.ModelAdmin):
    fields = ("user", "first_name", "last_name", 'age', "created_at")
    readonly_fields = ("created_at",)
    search_fields = ("first_name", "last_name")
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+   list_display = ("user", "country", "city", 'address', "created_at")
+   fields = ("user", "country", "city", 'address', "created_at")
+   readonly_fields = ("created_at",)
+   search_fields = ("user", "address")
--- shop/profiles/models.py
+++ shop/profiles/models.py
@@ -12,3 +12,21 @@ class Profile(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True, db_index=True
     )
+
+    def __str__(self):
+        return f'Profile {self.user}: {self.first_name} {self.last_name}'
+
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="addresses"
+    )
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
+
+    def __str__(self):
+        return f'Address {self.user}: {self.city}, {self.address}'
