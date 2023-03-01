3d95fa4aa3572adb26106f40a91004f880623271
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-02 00:05:57 +0300

    Update products

--- shop/products/models.py
+++ shop/products/models.py
@@ -31,3 +31,6 @@ class Purchase(models.Model):
     count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
+    def __str__(self):
+        return f"Purchase: {self.user} - {self.product} - {self.count}"
+
