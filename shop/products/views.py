349988d7990bd7cbef69898b06d637ec2d84827f
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-13 03:25:54 +0300

    Update products

--- shop/products/views.py
+++ shop/products/views.py
@@ -2,6 +2,7 @@
 import logging
 from django.http import HttpResponse
 
+from django.db.models import Max, Sum, Count
 from products.models import Product, Purchase
 logger = logging.getLogger(__name__)
 
@@ -15,6 +16,11 @@ def index(request):
     purchases__count = request.GET.get('purchases__count')
     if purchases__count is not None:
         products = products.filter(purchases__count=purchases__count)
+
+    # queryset = Product.objects.annotate(purchase__count=Count('purchases'))
+    # queryset = queryset.filter(purchase_count_gt=0)
+    # print(queryset.first().purchase_count)
+
 
     return HttpResponse('<br>'.join([str(p) for p in products]))
 
