35bebbe1ccab7bb604163d69ee78f3f1c526fc5b
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-17 23:39:42 +0300

    Update profiles

--- shop/profiles/forms.py
+++ shop/profiles/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 
 
-class RegisterForm(forms.Form):
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
+class RegisterUserForm(forms.Form):
+    username = forms.CharField(max_length=20)
     email = forms.EmailField()
     password = forms.CharField(
         min_length=8, widget=forms.PasswordInput()
     )
+
+class RegisterProfileForm(forms.Form):
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
     age = forms.IntegerField(min_value=18, required=False)
--- shop/profiles/views.py
+++ shop/profiles/views.py
@@ -1,8 +1,12 @@
 import logging
+
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from profiles.forms import RegisterForm
+from profiles.forms import RegisterUserForm
+from profiles.forms import RegisterProfileForm
+from profiles.models import Profile
 
 logger = logging.getLogger(__name__)
 
@@ -13,16 +17,31 @@ def profiles(request):
     return HttpResponse("Profiles view")
 
 
-def register(request):
+def register_user(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
-            # User.objects.create()
+            User.objects.create(username=form.cleaned_data['username'],
+                                email=form.cleaned_data['email'],
+                                password=form.cleaned_data['password'])
             logger.info(f"User email: {form.cleaned_data['email']}")
             logger.info(f"User password: {form.cleaned_data['password']}")
             return redirect("/")
     else:
-        form = RegisterForm()
+        form = RegisterUserForm()
 
-    return render(request, "register.html", {"form": form})
+    return render(request, "register_user.html", {"form": form})
 
+def register_profile(request):
+    if request.method == 'POST':
+        form = RegisterProfileForm(request.POST)
+        if form.is_valid():
+            Profile.objects.create(first_name=form.cleaned_data['first_name'],
+                                   last_name=form.cleaned_data['last_name'],
+                                   age=form.cleaned_data['age']
+                                   )
+            return redirect('/')
+    else:
+        form = RegisterProfileForm()
+
+    return render(request, 'register_profile.html', {'form': form})
