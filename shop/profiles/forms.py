9ed61e6b01e0c4854f8dcc3980bdec96a04cd216
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-24 01:00:47 +0300

    Update profiles files

--- shop/profiles/forms.py
+++ shop/profiles/forms.py
@@ -12,3 +12,10 @@ class RegisterProfileForm(forms.Form):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
     age = forms.IntegerField(min_value=18, required=False)
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(
+        min_length=8, widget=forms.PasswordInput()
+    )
--- shop/profiles/views.py
+++ shop/profiles/views.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from profiles.forms import RegisterUserForm
+from profiles.forms import RegisterUserForm, LoginForm
 from profiles.forms import RegisterProfileForm
 from profiles.models import Profile
+
+from django.contrib.auth import logout, login, authenticate
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,12 +24,13 @@ def register_user(request):
     if request.method == "POST":
         form = RegisterUserForm(request.POST)
         if form.is_valid():
-            User.objects.create(username=form.cleaned_data['username'],
-                                email=form.cleaned_data['email'],
-                                password=form.cleaned_data['password'])
-            logger.info(f"User email: {form.cleaned_data['email']}")
-            logger.info(f"User password: {form.cleaned_data['password']}")
-            return redirect("/register_profile")
+            user = User(
+                email=form.cleaned_data["email"],
+                username=form.cleaned_data['email'],
+            )
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("/login")
     else:
         form = RegisterUserForm()
 
@@ -36,7 +40,7 @@ def register_profile(request):
     if request.method == 'POST':
         form = RegisterProfileForm(request.POST)
         if form.is_valid():
-            Profile.objects.create(user_id=User.id,
+            Profile.objects.create(user=request.user,
                                    first_name=form.cleaned_data['first_name'],
                                    last_name=form.cleaned_data['last_name'],
                                    age=form.cleaned_data['age']
@@ -46,3 +50,25 @@ def register_profile(request):
         form = RegisterProfileForm()
 
     return render(request, 'register_profile.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                request=request,
+                username=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            if user is None:
+                return HttpResponse('BadRequest', status=400)
+            login(request, user)
+            return redirect("index")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")
