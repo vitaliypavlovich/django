cb7408810fcb2d4e7850c5e948de9659331cf331
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-28 23:12:17 +0300

    Add tests

---
+++ shop/tests/test_products.py
@@ -0,0 +1,12 @@
+import pytest
+from django.test.client import Client
+
+
+@pytest.mark.django_db
+class TestIndex:
+    def setup_method(self):
+        self.client = Client()
+
+    def test_my_function(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
---
+++ shop/tests/test_register.py
@@ -0,0 +1,21 @@
+import pytest
+from django.contrib.auth.models import User
+from django.test.client import Client
+
+
+@pytest.mark.django_db
+class TestIndex:
+    def setup_method(self):
+        self.client = Client()
+
+    def test_my_function(self):
+        response = self.client.get("/register_user/")
+        assert response.status_code == 200
+
+        response = self.client.post("/register_user/", data={
+            "username": "test",
+            "email": "test@test.com",
+            "password": "Test",
+        }, follow=True)
+        assert response.status_code == 200
+        assert User.objects.exists()
