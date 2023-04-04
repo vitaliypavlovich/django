7ea38c629de7647680dcd6cb737df4748e012a4a
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-05 02:32:25 +0300

    Update urls api

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from api.products.views import ProductViewSet
+from api.users.login_views import LoginView
+from api.users.views import RegisterView
 
 app_name = "api"
 
@@ -10,4 +12,6 @@ router.register(r"products", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login")
 ]
