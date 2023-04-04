71f1e8114e669f422cc7f536f57e3989f36ceb9d
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-05 02:30:47 +0300

    Update homework

--- notes/apii/memorandum/serializers.py
+++ notes/apii/memorandum/serializers.py
@@ -3,8 +3,7 @@ from rest_framework import serializers
 from memorandum.models import Note
 
 
-
 class NoteModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Note
-        fields = ["title", "text", "author", "created_at"]
+        fields = ["title", "text", "created_at"]
