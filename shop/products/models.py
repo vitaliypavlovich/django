285d7f9739c205180537a87368b783f687f67ef6
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-05 23:10:43 +0300

    Update products models

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.conf import settings
 from django.db import models
 
@@ -9,10 +11,12 @@ COLOR_CHOICES =(
 )
 
 class Product(models.Model):
+    external_id = models.CharField(max_length=255, blank=True, null=True)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="products/", blank=True, null=True)
     color = models.CharField(max_length=32, choices=COLOR_CHOICES, default='RED')
-    price = models.IntegerField(default=0)
+    price = models.DecimalField(default=Decimal("0"), decimal_places=5, max_digits=10)
+    excerpt = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(
         auto_now_add=True, db_index=True
