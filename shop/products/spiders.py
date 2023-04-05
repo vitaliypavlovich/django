39c9ad780465b39faee4a5069441d1f92804987e
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-05 23:11:02 +0300

    Add spiders

---
+++ shop/products/spiders.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class OmaSpider(scrapy.Spider):
+    name = "oma.by"
+    allowed_domains = ["https://www.oma.by"]
+    start_urls = ["https://www.oma.by/elektroinstrument-c"]
+
+    def parse(self, response, **kwargs):
+        for product in response.css(".catalog-grid .product-item"):
+            data = {
+                "external_id": int(product.attrib.get("data-ga-product-id")),
+                "name": product.attrib.get("data-ga-product-name").strip(),
+                "price": product.attrib.get("data-ga-product-price").strip(),
+                "category": product.attrib.get("data-ga-category-last").strip(),
+                "link": f"{self.allowed_domains[0]}{product.css('a.area-link::attr(href)').get()}",
+            }
+            yield data
