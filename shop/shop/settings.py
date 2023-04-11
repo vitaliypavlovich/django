195a3d467bcdc432ccb2806871b9dfa6e5c8ba4d
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-12 01:25:12 +0300

    Add cache connection

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -93,6 +93,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
+
 
 
 
