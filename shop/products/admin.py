7e9715490fba81f8593c61faa3fe0dca0238f932
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-12 01:24:11 +0300

    Update products admin

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -15,9 +15,15 @@ class ProductAdmin(admin.ModelAdmin):
    search_fields = ("title", 'category', "price")
    inlines = (PurchaseAdminInline,)
 
+   def save_form(self, request, form, change):
+
+      return super().save_form(request, form, change)
+
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
    list_display = ("user", "product", "count", "created_at")
    fields = ("user", "product", "count", "created_at")
    readonly_fields = ("created_at",)
    search_fields = ("user__email", "product__title")
+
+
