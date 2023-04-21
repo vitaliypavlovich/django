8ae4af7654ef624643388c3d6745ff14686d1a48
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-04-21 10:42:13 +0300

    Move spider to worker

--- shop/products/management/commands/oma_spider.py
+++ shop/products/management/commands/oma_spider.py
@@ -8,21 +8,29 @@ from scrapy.utils.project import get_project_settings
 from scrapy.signalmanager import dispatcher
 from scrapy import signals
 
+from django_rq import job
+
+@job
+def run_spider():
+    Product.objects.all().delete()
+
+    def crawler_results(signal, sender, item, response, spider):
+        Product.objects.update_or_create(external_id=item["external_id"], defaults={
+            "title": item["name"],
+            "price": item["price"],
+            "description": item["link"],
+            "category": item["category"],
+        })
+
+    dispatcher.connect(crawler_results, signal=signals.item_scraped)
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(OmaSpider)
+    process.start()
+
 
 class Command(BaseCommand):
     help = "Crawl OMA catalog"
 
     def handle(self, *args, **options):
-        def crawler_results(signal, sender, item, response, spider):
-            Product.objects.update_or_create(external_id=item["external_id"], defaults={
-                "title": item["name"],
-                "price": item["price"],
-                "description": item["link"],
-                "category": item["category"],
-            })
-        dispatcher.connect(crawler_results, signal=signals.item_scraped)
-
-        process = CrawlerProcess(get_project_settings())
-        process.crawl(OmaSpider)
-        process.start()
-
+        run_spider.delay()
