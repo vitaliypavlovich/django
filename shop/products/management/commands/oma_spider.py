8fca24242a3796c83492972b901a195d683ca85c
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-05-03 15:18:55 +0300

    Add products image

--- shop/products/management/commands/oma_spider.py
+++ shop/products/management/commands/oma_spider.py
@@ -1,3 +1,5 @@
+import requests
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from products.models import Product
@@ -15,11 +17,15 @@ def run_spider():
     Product.objects.all().delete()
 
     def crawler_results(signal, sender, item, response, spider):
+        image_name = item['image_name'].split('/')[-1]
+        response = requests.get(item['image_name'])
+        open(settings.MEDIA_ROOT / 'products' / image_name, 'wb').write(response.content)
         Product.objects.update_or_create(external_id=item["external_id"], defaults={
             "title": item["name"],
             "price": item["price"],
             "description": item["link"],
             "category": item["category"],
+            'image': f'products/{image_name}'
         })
 
     dispatcher.connect(crawler_results, signal=signals.item_scraped)
--- shop/products/spiders.py
+++ shop/products/spiders.py
@@ -5,18 +5,24 @@ class OmaSpider(scrapy.Spider):
     name = "oma.by"
     allowed_domains = ["www.oma.by"]
     start_urls = ["https://www.oma.by/elektroinstrument-c"]
+    current_page = 1
 
     def parse(self, response, **kwargs):
         for product in response.css(".catalog-grid .product-item"):
+            image_div = product.css(".product-item_img-box .image-filler:first-child")
+            image_name = f"https://www.oma.by{image_div.attrib.get('data-img-src')}"
             data = {
                 "external_id": int(product.attrib.get("data-ga-product-id")),
                 "name": product.attrib.get("data-ga-product-name").strip(),
                 "price": product.attrib.get("data-ga-product-price").strip(),
                 "category": product.attrib.get("data-ga-category-last").strip(),
+                'image_name': image_name,
                 "link": f"{self.allowed_domains[0]}{product.css('a.area-link::attr(href)').get()}",
             }
             yield data
 
         next_page = response.css(".page-nav_box .btn__page-nav:last-child::attr(href)").get()
         if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+            self.current_page += 1
+            if self.current_page == 2:
+                yield response.follow(next_page, callback=self.parse)
