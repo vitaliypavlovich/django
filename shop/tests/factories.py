4db12335b4fe2fae2f88fc5e2841c8b33938cec3
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-05-09 23:07:59 +0300

    Add factories

--- shop/tests/factories.py
+++ shop/tests/factories.py
@@ -2,9 +2,13 @@ import factory
 
 from factory.django import DjangoModelFactory
 
-from products.models import Product
+from products.models import Product, Purchase
 
 from decimal import Decimal
+
+from profiles.models import Profile
+
+from random import randint
 
 class ProductFactory(DjangoModelFactory):
     class Meta:
@@ -14,3 +18,21 @@ class ProductFactory(DjangoModelFactory):
     color = 'RED'
     price = Decimal(290)
     price_usd = Decimal(100)
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = Profile
+
+    first_name = factory.Faker('word')
+    last_name = factory.Faker('hello')
+    age = randint(1, 50)
+
+
+class PurchaseFactory(DjangoModelFactory):
+    class Meta:
+        model = Purchase
+
+    user = factory.Faker('word')
+    product = 'RED'
+    count = Decimal(290)
