824882e5adc7c3a87cb35535facb488623a60566
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-05-09 22:58:57 +0300

    Add test with factory

--- shop/tests/api/test_products.py
+++ shop/tests/api/test_products.py
@@ -1,6 +1,8 @@
 import pytest
 from rest_framework.test import APIClient
 
+from tests.factories import ProductFactory
+
 
 @pytest.mark.django_db
 class TestProductsApi:
@@ -11,3 +13,9 @@ class TestProductsApi:
         response = self.client.get("/api/products/")
         assert response.status_code == 200
         assert len(response.json()) == 0
+
+    def test_popular(self):
+        ProductFactory.create_batch(10)
+        response = self.client.get("/api/products/popular/")
+        assert response.status_code == 200
+        assert response.json().get('count') == 10
