2b51c2bb283ab43758c1f4c23718a80572fd6d1c
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-05-31 15:30:15 +0300

    Update static and  media config

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ROOT_DIR = BASE_DIR.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "my-super-secret-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -156,10 +156,10 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = None
+STATIC_ROOT = ROOT_DIR / 'static'
 STATIC_URL = "static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = ROOT_DIR / "media"
 MEDIA_URL = "media/"
 
 # Default primary key field type
